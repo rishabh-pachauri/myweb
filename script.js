f0090386cfe699672dbfcd5c4d335881654622fd
rishabh-pachauri <69377085+rishabh-pachauri@users.noreply.github.com>
2025-05-20 14:09:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+  menu.classList.toggle("active");
+  icon.classList.toggle("active");
 }
 if (window.location.pathname.endsWith(".html")) {
   window.history.replaceState(null, "", window.location.pathname.replace(".html", ""));
